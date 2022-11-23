4cf5e1dab33c6b12d17cb40547560d384bf0174c
sajid9962 <clashking1545@gmail.com>
2022-11-23 22:32:18 +0600

    connection with database established

--- index.js
+++ index.js
@@ -11,14 +11,24 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(express.json());
 app.use(cors());
 
+// database
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.59reojt.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
 app.get('/', (req, res) => {
-res.send('Laptop city server running');
+	res.send('Laptop city server running');
 });
 
 app.listen(port, () => {
-console.log(`Running server on ${port}`);
+	console.log(`Running server on ${port}`);
 });
